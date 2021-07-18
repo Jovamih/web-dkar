e18be81f4f3afd768cc94e692b6c609cb62418e9
johan_mitma12 <avira365@hotmail.com>
2021-07-18 00:20:19 -0500

    se agregaron referencias iniciales de la pagina web

---
+++ login_admin/login.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <title>Login</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Transparent Login Form Responsive Widget,Login form widgets, Sign up Web forms , Login signup Responsive web form,Flat Pricing table,Flat Drop downs,Registration Forms,News letter Forms,Elements" />
+    <!--web-fonts-->
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css' />
+    <!--web-fonts-->
+</head>
+
+<body >
+<!--header-->
+	<div class="header-w3l">
+		<h1> Login de Dueño de Boutique</h1>
+	</div>
+<!--//header-->
+
+<!--main-->
+<div class="main-content-agile">
+	<div class="sub-main-w3">	
+		<form action="login.php" method="post">
+			<input placeholder="Username or E-mail" name="Name" class="user" type="text" required="" style="background-color:transparent;"><br>
+			<input  placeholder="Password" name="Password" class="pass" type="password" required=""><br>
+			<input type="submit" value="">
+		</form>
+	</div>
+</div>
+<!--//main-->
+
+<!--footer-->
+<div class="footer">
+	<p>Footer en adopcion</p>
+</div>
+<!--//footer-->
+
+</body>
+</html>
