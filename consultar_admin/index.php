36212a554e240a7a84d86ab679f5ae92c357cde8
johan_mitma12 <avira365@hotmail.com>
2021-08-01 02:09:19 -0500

    favicon agregado

--- consultar_admin/index.php
+++ consultar_admin/index.php
@@ -15,6 +15,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="https://kit.fontawesome.com/31127b7562.js" crossorigin="anonymous"></script>
+
+    <link rel="icon" type="image/png" href="../resources/faviconv2.png"/>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
